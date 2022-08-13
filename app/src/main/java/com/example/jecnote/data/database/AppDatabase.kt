09e9b1123c20e68fabd925f7d66b2b3ab9f3f7d5
bobowg <bobowg@163.com>
2022-08-13 08:24:56 +0800

    three filp

--- app/src/main/java/com/example/jecnote/data/database/AppDatabase.kt
+++ app/src/main/java/com/example/jecnote/data/database/AppDatabase.kt
@@ -1,10 +1,17 @@
 package com.example.jecnote.data.database
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.jecnote.data.database.dao.ColorDao
+import com.example.jecnote.data.database.dao.NoteDao
 import com.example.jecnote.data.database.model.ColorDbModel
 import com.example.jecnote.data.database.model.NoteDbModel
 
-@Database(entities = [NoteDbModel::class, ColorDbModel::class], version = 1)
-abstract class AppDatabase {
-
+@Database(entities = [NoteDbModel::class, ColorDbModel::class], version = 1, exportSchema = false)
+abstract class AppDatabase :RoomDatabase(){
+    companion object{
+        const val DATABASE_NAME = "note-maker-database"
+    }
+    abstract fun noteDao():NoteDao
+    abstract fun colorDao():ColorDao
 }
